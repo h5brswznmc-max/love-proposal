7ed8538bc240d0271101f12399d913162c8ee3b7
h5brswznmc-max <h5brswznmc@privaterelay.appleid.com>
2026-07-29 04:17:53 +0400

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -10,54 +10,55 @@ const images = [
   "https://i.ibb.co/SwsxZpJm/MG-2211.jpg",
   "https://i.ibb.co/mLwZZCz/MG-2092.jpg",
   "https://i.ibb.co/KzS2Zk8K/MG-2086.jpg",
-  "https://i.ibb.co/WNkkJbyp/MG-2027.jpg",
+  "https://i.ibb.co/WNkkJbyp/MG-2027.jpg"
 ];
 
 const texts = [
-
-"Hər dəfə sənə baxanda ürəyim daha sürətli döyünür... ❤️",
-
-"Sənin gülüşün mənim ən sevdiyim mənzərədir. ✨",
-
-"Həyatımın ən gözəl təsadüfü səni tanımağım oldu. 💖",
-
-"İndi isə sənə ürəyimdə saxladığım ən vacib sualı verməyin vaxtıdır... ❤️"
-
+  "Hər dəfə sənə baxanda ürəyim daha sürətli döyünür... ❤️",
+  "Sənin gülüşün mənim ən sevdiyim mənzərədir. ✨",
+  "Həyatımın ən gözəl təsadüfü səni tanımağım oldu. 💖",
+  "Səninlə keçirdiyim hər an mənim üçün çox dəyərlidir. 🥰",
+  "Sən həyatımı gözəlləşdirən insansan. 🌹",
+  "Sənin yanında özümü xoşbəxt hiss edirəm. ❤️",
+  "Gələcəyimi səninlə birlikdə xəyal edirəm. 💍",
+  "Səni hər gün daha çox sevirəm. 💕",
+  "İndi isə sənə ürəyimdə saxladığım ən vacib sualı verməyin vaxtıdır... ❤️"
 ];
 
 let index = 0;
 
-function changePhoto(){
+// İlk şəkil və mətn
+photo.src = images[0];
+message.innerHTML = texts[0];
+
+function changePhoto() {
 
     photo.style.opacity = "0";
     message.style.opacity = "0";
 
-    setTimeout(()=>{
+    setTimeout(() => {
 
         index++;
 
-        if(index >= photos.length){
+        if (index >= images.length) {
 
-            document.body.style.transition="1.5s";
-            document.body.style.opacity="0";
+            document.body.style.transition = "opacity 1.5s";
+            document.body.style.opacity = "0";
 
-            setTimeout(()=>{
-
-                location.href="letter.html";
-
-            },1500);
+            setTimeout(() => {
+                location.href = "letter.html";
+            }, 1500);
 
             return;
         }
 
-        photo.src = photos[index];
+        photo.src = images[index];
         message.innerHTML = texts[index];
 
-        photo.style.opacity="1";
-        message.style.opacity="1";
+        photo.style.opacity = "1";
+        message.style.opacity = "1";
 
-    },700);
-
+    }, 700);
 }
 
-setInterval(changePhoto,6000);
+setInterval(changePhoto, 6000);
