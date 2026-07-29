245592f5d12948c33e39e28042f0a284158fb5c8
h5brswznmc-max <h5brswznmc@privaterelay.appleid.com>
2026-07-29 04:41:42 +0400

    Update script.js

--- script.js
+++ script.js
@@ -3,29 +3,16 @@ const music = document.getElementById("music");
 
 start.addEventListener("click", async () => {
 
-    try {
-        await music.play();
-        music.volume = 0.6;
-    } catch (e) {
-        console.log(e);
-    }
-
-    document.body.style.opacity = "0";
-
-    setTimeout(() => {
-        window.location.href = "gallery.html";
-    }, 1000);
-
-});
-
-start.addEventListener("click", async () => {
-
     start.disabled = true;
     start.innerHTML = "❤️ Hazır ol... ❤️";
 
-    try{
+    music.volume = 0.6;
+
+    try {
         await music.play();
-    }catch(e){}
+    } catch (e) {
+        console.log(e);
+    }
 
     document.body.style.transition = "1.5s ease";
     document.body.style.filter = "brightness(0)";
@@ -33,13 +20,6 @@ start.addEventListener("click", async () => {
 
     setTimeout(() => {
         window.location.href = "gallery.html";
-    },1500);
+    }, 1500);
 
 });
-.glass{
-    transition:1s;
-}
-
-body{
-    transition:1.5s;
-}
